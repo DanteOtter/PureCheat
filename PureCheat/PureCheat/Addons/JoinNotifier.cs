217b9e986355ff46e4bf84ff46f4ee9ce1649b89
DanteOtter <54197896+DanteOtter@users.noreply.github.com>
2021-02-01 11:10:23 -0600

    Update JoinNotifier.cs
    Grab the name field and return username.

--- PureCheat/PureCheat/Addons/JoinNotifier.cs
+++ PureCheat/PureCheat/Addons/JoinNotifier.cs
@@ -1,4 +1,4 @@
-﻿using VRC;
+using VRC;
 using PureCheat.API;
 
 namespace PureCheat.Addons
@@ -10,13 +10,13 @@ namespace PureCheat.Addons
         public override void OnPlayerJoin(Player player)
         {
             string playerName = player.gameObject.GetComponent<VRCPlayer>().ToString();
-            PureLogger.Log($"{playerName} joined room!");
+            PureLogger.Log($"{player.field_Private_APIUser_0.displayName} joined the room!");
         }
 
         public override void OnPlayerLeave(Player player)
         {
             string playerName = player.gameObject.GetComponent<VRCPlayer>().ToString();
-            PureLogger.Log($"{playerName} leaved room!");
+            PureLogger.Log($"{player.field_Private_APIUser_0.displayName} left the room!");
         }
     }
 }
